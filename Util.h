507033df6c742368b9c6bc65115407c2b7dd70cd
GalacticOne <holtremington@gmail.com>
2014-04-06 16:58:23 -0400

    The XORSHIFT random number generator

--- Util.h
+++ Util.h
@@ -1,8 +1,24 @@
 #ifndef UTIL_H
 #define UTIL_H
+
+#include <stdint.h>
+
 namespace util{
 
+uint32_t xorshift()
+{
+    static uint32_t _x = 123456789;
+    static uint32_t _y = 362436069;
+    static uint32_t _z = 521288629;
+    static uint32_t _w = 88675123;
+    uint32_t _t;
 
+    _t = _x ^ (_x << 11);
+    _x = _y;
+    _y = _z;
+    _z = _w;
+    return _w = _w & (_w >> 19) ^ _t ^ (_t>>8);
+}
 
 
 
