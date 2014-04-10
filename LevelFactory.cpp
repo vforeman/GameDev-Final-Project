ad61ea84dc8b6b6286e8e749a4a19a2b717b92ba
Victor Foreman <vforeman@scs.howard.edu>
2014-04-10 19:34:49 -0400

    spec and imp for lvlfact

--- LevelFactory.cpp
+++ LevelFactory.cpp
@@ -1,12 +1,14 @@
 #include "LevelFactory.h"
 namespace level{
 
-
 LevelContainer LevelFactory::_levelContainer ={
   "Main Static Level Container"
 };
 
-
+LevelFactory::LevelFactory(){}
+LevelFactory::~LevelFactory(){}
+LevelFactory * LevelFactory::get(){}
+LevelContainer * LevelFactory::getLevel(string lvl){}
 
 
 }
