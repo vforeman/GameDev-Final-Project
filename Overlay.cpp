7690c87ad96fff078a8754b6665657f24c34e0eb
Ranjay <ranjy23@gmail.com>
2014-04-12 20:37:08 -0400

    fixed errors in overlay and level

--- Overlay.cpp
+++ Overlay.cpp
@@ -1,13 +1,81 @@
 #include "Overlay.h"
-
-
+char Overlay::_overlay[][];
 Overlay::Overlay()
-{}
+{
+	Initialize();
+};
 Overlay::~Overlay()
-{}
-bool Overlay::isObstacle(Vector3f)
-{}
-bool Overlay::isObstacle(int x, int y, int z)
-{}
-void Overlay::constructQuadrant()
-{}
+{
+
+};
+
+/*char Overlay::getObstacle(float radius,Vector3<float> pos)
+{
+    //A lot of variables for checking radius of sphere
+    //For loop crashed the game
+    char val = 'F';
+    float x = pos.x;
+    float z = pos.z;
+    unsigned int i = 0;
+    unsigned int j = 0;
+     
+            i = std::abs((int)roundf(x))/10;
+            j = std::abs((int)roundf(z))/10;
+
+            if(i >= 10 || j >= 10 || i < 0 || j < 0)
+                return 'W';
+
+            if(x >= 0.0f && z >= 0.0f)
+               if(_overlay[i][j] == 'W')
+                    return 'W';
+
+            if(x <= 0.0f && z >=0.0f)
+               if(_overlay[i][j] == 'W')
+                    return 'W';
+
+            if(x <= 0.0f && z <= 0.0f)
+               if(_overlay[i][j] == 'W')
+                    return 'W';
+
+            if(x >= 0.0f && z <= 0.0f)
+               if(_overlay[i][j] == 'W')
+                    return 'W';
+
+    return val;
+} */
+
+/*bool Overlay::isObstacle(Vector3<float>)
+{
+	return;
+};
+bool Overlay:: isObstacle(int, int, int)
+{
+
+};
+*/
+
+void Overlay::Initialize()
+{
+	for(unsigned int i = 0; i < 20; ++i)
+        for(unsigned int j = 0; j < 20; ++j)
+            _overlay[i][j] = 'F';
+
+	 for(unsigned int i = 0; i < 5; ++i)
+        Generator::Random(); // Random function from util class instead of generator, Random function was a intermediate call to the Rand() function
+
+	ConstructQuadrant();
+
+
+};
+
+void Overlay::ConstructQuadrant()
+{
+    
+    for(unsigned int i = 0; i < 20; ++i)
+        for(unsigned int j = 0; j < 20; ++j)
+        {
+            if(Generator::Random() % 100 >= 90)
+                _overlay[i][j] = 'W';
+        }
+};
+
--- src/Level.h
+++ src/Level.h
@@ -1,19 +1,27 @@
-#ifndef LEVEL_H
-#define lEVEL_H
+#ifndef LEVELTEST_H
+#define LEVELTEST_H
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
 #include <vector>
-using namespace std;
+#include "Generator.h"
+//#include "Constants.h"
+#include "Overlay.h"
+
 class Level
 {
-public:
-Level();
-~Level();
-void initialize();
-vector<float> _wallPoints;
-private:
-
-
+	public:
+		Level();
+		void buildLevel(float x, float z, unsigned int i, unsigned int j);
+		void CreateWallPoints(Overlay);
+	private:
+		std::vector<float> _wallPoints;
+		Overlay createOverLay;
 };
-
 
 
 
