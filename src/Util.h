e5c32b2a9e885170efcc55e933b3aeb6ec867b85
GalacticOne <holtremington@gmail.com>
2014-04-21 08:16:13 -0400

    Added Random Library

--- src/Util.h
+++ src/Util.h
@@ -9,27 +9,11 @@
 #include <cstdlib>
 #include <cstdint>
 #include <typeinfo>
+#include <random>
 
 using namespace std;
 namespace util{
 static bool init = false;
-
-
-inline uint32_t xorshift()
-{
-
- static uint32_t _x = 123456789;
- static uint32_t _y = 362436069;
- static uint32_t _z = 521288629;
- static uint32_t _w = 88675123;
- uint32_t _t;
-
- _t = _x ^ (_x << 11);
- _x = _y;
- _y = _z;
- _z = _w;
- return _w = _w ^ (((_w >> 19) ^ _t) ^ (_t>>8));
-}
 
 inline bool coinToss(){
 	if(!init){
