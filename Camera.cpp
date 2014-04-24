5cfbb522d94f514b1bba04d7ff78f9445e27523d
GalacticOne <holtremington@gmail.com>
2014-04-24 02:42:43 -0400

    Code Clean

--- Camera.cpp
+++ Camera.cpp
@@ -65,26 +65,6 @@ void Camera::moveCamera(const float &dir)
     loc.x-=sin(rad)*speed;
 	loc.z-=cos(rad)*speed;
   
-  /*  
-    //Recalculate forward vector based off new yaw and pitch
-    _forward.x =  sin(camYaw*degrees2Radians) * cos(camPitch*degrees2Radians);
-    _forward.y =  sin(camPitch * degrees2Radians);
-    _forward.z = -cos(camYaw * degrees2Radians) * cos(camPitch * degrees2Radians);
-    _forward.normalize();
-
-    //Recalculate up vector based off new yaw and pitch
-    _up.x = sin(camYaw * degrees2Radians) * cos((camPitch+90.0f) * degrees2Radians);
-    _up.y = sin((camPitch+90.0f) * degrees2Radians);
-    _up.z = -cos(camYaw*degrees2Radians) * cos((camPitch+90.0f)*degrees2Radians);
-    _up.normalize();
-
-    //Recalculate right vector based off new 
-    _right.x = sin((camYaw+90.0f) * degrees2Radians);
-    _right.y = 0.0f;
-    _right.z = -cos((camYaw+90.0f)*degrees2Radians);
-    _right.normalize();
-  */
-  
     if(Overlay::isObstacle(loc.x, loc.z)) 
     {
         loc.x = x;
@@ -115,7 +95,6 @@ void Camera::control()
 
 
     //Recalculate forward vector based off new yaw and pitch
-    //_forward.x =  sin(camYaw*degrees2Radians) * cos(camPitch*degrees2Radians);
     _forward.x =  -sin(camYaw*degrees2Radians) * cos(camPitch*degrees2Radians);
     _forward.y =  sin(camPitch * degrees2Radians);
     _forward.z = -cos(camYaw * degrees2Radians) * cos(camPitch * degrees2Radians);
@@ -201,18 +180,6 @@ void Camera::mouseOut()
 //returns where the player is looking
 Vector3<float> Camera::getVector()
 {
-    /*
-    Vector3f z_axis = (loc - Vector3f(0.0f, 0.8f, 0.0f));
-    z_axis.normalize();
-    Vector3f up = Vector3f(0.0f, 1.0f, 0.0f);
-    Vector3f x_axis = up.crossProduct(z_axis);
-    x_axis.normalize();
-    Vector3f y_axis = z_axis.crossProduct(x_axis);
-
-    Vector3f eye =      Vector3f(x_axis.x, y_axis.x, z_axis.x);
-    Vector3f target =   Vector3f(x_axis.y, y_axis.y, z_axis.y);
-    */
-
 	// change spherical coordinates to cartesian
     //pitch = latitude = phi, yaw = longitude = theta
     //tutorial.math.lamar.edu/Classes/CalcIII/SphericalCoords.aspx
@@ -220,11 +187,6 @@ Vector3<float> Camera::getVector()
     float yaw = (camYaw);       //forward yaw
     
     //Closer Translation of spherical to rectangular coordinates
-    //Vector3f val = Vector3f(sin(pitch)*cos(yaw), sin(pitch)*sin(yaw), cos(pitch));
-    
-    //printf("%s\n", val.toString().c_str());
-
-    //return Vector3f(cos(yaw)*cos(yaw), sin(yaw)*cos(pitch), sin(pitch));
     
     float xzLen = cos(pitch);
     float x = xzLen * cos(yaw);
@@ -250,11 +212,6 @@ Vector3f Camera::getEye()
 
 Vector3f Camera::getForward()
 {
-     Vector3f location = loc;
-     location.normalize();
-     printf("Location: %s\n", loc.toString().c_str());
-     printf("LocNorm: %s\n", location.toString().c_str());
-     printf("Forward: %s\n", _forward.toString().c_str());
      return _forward;
 }
 
